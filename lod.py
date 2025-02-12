ff9e9d1e033348192fbd2ba9eae73fe53ba79be9
František Němeček <nemecekf24@spstrutnov.cz>
2025-02-12 08:50:25 +0100

    nepřátelé mizí po kolizi se střelou

--- lod.py
+++ lod.py
@@ -1,4 +1,6 @@
 import pygame
+import random
+import math
 
 pygame.init()
 
@@ -9,6 +11,7 @@ pygame.display.set_caption("Ovládání trojúhelníku")
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 BLUE = (0, 0, 255)
+BROWN = (139, 69, 19)
 
 x = WIDTH // 2
 y = HEIGHT - 50
@@ -16,6 +19,16 @@ speed = 5
 
 bullets = []
 bullet_speed = 7
+
+enemies = []
+for _ in range(5):
+    enemy_x = random.randint(20, WIDTH - 20)  
+    enemy_y = random.randint(20, 100)         
+    enemies.append((enemy_x, enemy_y))
+
+def is_collision(bullet, enemy):
+    distance = math.hypot(bullet[0] - enemy[0], bullet[1] - enemy[1])
+    return distance < 15
 
 running = True
 while running:
@@ -47,6 +60,13 @@ while running:
         bullet[1] -= bullet_speed  
 
     bullets = [bullet for bullet in bullets if bullet[1] > 0]
+    
+    for bullet in bullets[:]:  
+        for enemy in enemies[:]:
+            if is_collision(bullet, enemy):
+                bullets.remove(bullet)  
+                enemies.remove(enemy)  
+                break 
 
     points = [(x, y), (x + 15, y + 30), (x - 15, y + 30)]
     pygame.draw.polygon(screen, BLACK, points)
@@ -54,6 +74,9 @@ while running:
     for bullet in bullets:
         pygame.draw.circle(screen, BLUE, (bullet[0], bullet[1]), 5)
 
+    for enemy in enemies:
+        pygame.draw.circle(screen, BROWN, enemy, 15)
+        
     pygame.display.update()
 
 pygame.quit()
