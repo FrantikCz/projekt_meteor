f4c8996cc7b6c336d54a5ffc4d18d93513ec53af
František Němeček <nemecekf24@spstrutnov.cz>
2025-02-20 10:46:07 +0100

    přidáni pozadí vesmíru

--- lod.py
+++ lod.py
@@ -3,13 +3,15 @@ import random
 import math
 
 pygame.init()
-
 WIDTH, HEIGHT = 500, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Ovládání trojúhelníku")
 
+background = pygame.image.load("pozadi_vesmir.png")
+background = pygame.transform.scale(background, (WIDTH, HEIGHT))
+
 WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
+BLACK = (255, 255, 255)
 BLUE = (0, 255, 255)
 BROWN = (238, 203, 173)
 
@@ -19,8 +21,8 @@ speed = 5
 
 bullets = []
 bullet_speed = 11
-
 enemies = []
+
 for _ in range(5):
     enemy_x = random.randint(20, WIDTH - 20)  
     enemy_y = random.randint(20, 100)         
@@ -31,36 +33,34 @@ def is_collision(bullet, enemy):
     return distance < 15
 
 running = True
+clock = pygame.time.Clock() 
+
 while running:
-    pygame.time.delay(20)
-    screen.fill(WHITE)
+    clock.tick(60)
     
     keys = pygame.key.get_pressed()
-    
     if keys[pygame.K_LEFT] and x - 15 > 0:
         x -= speed
-    
     if keys[pygame.K_RIGHT] and x + 15 < WIDTH:
         x += speed
 
     points = [(x, y), (x + 15, y + 30), (x - 15, y + 30)]
     pygame.draw.polygon(screen, BLACK, points)
 
-    pygame.display.update()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
-            
+            running = False  
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 bullets.append([x, y - 10])
+                
+    screen.blit(background, (0, 0))
                 
     for bullet in bullets:
         bullet[1] -= bullet_speed  
 
     bullets = [bullet for bullet in bullets if bullet[1] > 0]
-    
     for bullet in bullets[:]:  
         for enemy in enemies[:]:
             if is_collision(bullet, enemy):
@@ -70,14 +70,11 @@ while running:
 
     points = [(x, y), (x + 15, y + 30), (x - 15, y + 30)]
     pygame.draw.polygon(screen, BLACK, points)
-
     for bullet in bullets:
         pygame.draw.circle(screen, BLUE, (bullet[0], bullet[1]), 5)
-
     for enemy in enemies:
         pygame.draw.circle(screen, BROWN, enemy, 15)
-        
+          
     pygame.display.update()
 
 pygame.quit()
- 
